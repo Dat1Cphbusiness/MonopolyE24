6b7ae92e4c57a2ee040f4c1d3c7b106415c4ef2d
Maximitas <Lassetimm@outlook.dk>
2024-04-03 11:49:18 +0200

    Card and CardDeck

---
+++ src/Card.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Card {
+    private String message;
+    private int income;
+    private int cost;
+    private String event;
+
+    public Card(String message, int income, int cost, String event){
+
+        this.message = message;
+        this.income = income;
+        this.cost = cost;
+        this.event = event;
+    }
+
+}
---
+++ src/CardDeck.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class CardDeck{
+    private String[] carddata;
+    private ArrayList<Card> cards;
+    public CardDeck(String[] carddata, ArrayList<Card> cards){
+        this.carddata = carddata;
+        this.cards = new ArrayList<Card>();
+
+    }
+
+    public void createCards(String[] carddata){
+        for (int i = 0; i < carddata.length; i++) {
+            String[] values = carddata[i].split(",");
+            String message = values[0];
+            int income = Integer.parseInt(values[1]);
+            int cost = Integer.parseInt(values[2]);
+            String event = values[3];
+            Card c = new Card(message, income, cost, event);
+
+
+
+        }
+    }
+    public Card getNext(){
+        return null;
+    }
+
+}
