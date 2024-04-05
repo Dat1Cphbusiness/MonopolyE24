e8844d29cf4547e82a29842893c6fcd18829f6af
Gervig <gervig91@gmail.com>
2024-04-05 12:01:53 +0200

    Casper&Malou fields

--- src/Property.java
+++ src/Property.java
@@ -1,6 +1,8 @@
 public class Property extends Field{
-    public Property(int id, String label, int cost, int income) {
+    private int serieID;
+    public Property(int id, String label, int cost, int income, int serieID) {
         super(id, label, cost, income);
+        this.serieID = serieID;
 
     }
 
