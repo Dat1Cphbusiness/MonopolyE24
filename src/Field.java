03a0d433943489c6926e8154605f9b5434165c3d
MojoOno <Ffranck0406@gmail.com>
2024-04-08 14:58:23 +0200

    Field and property sprint 3 (r, r, m, f)

--- src/Field.java
+++ src/Field.java
@@ -3,7 +3,7 @@ public class Field {
     private String label;
     private int cost;
     private int income;
-    private String option;
+    protected String option;
 
     public Field(int id, String label, int cost, int income) {
         this.ID = id;
@@ -12,15 +12,29 @@ public class Field {
         this.income = income;
     }
 
+    public String processResponse(String response, Player p) {
+        if (response.equalsIgnoreCase("Y")) {
+            onAccept(p);
+        } else if (response.equalsIgnoreCase("N")) {
+            onReject(p);
+        } else {
+            System.out.println("Invalid input");
+            processResponse(response, p);
+        }
+        return "";
+    }
+
     @Override
     public String toString() {
         return "felt " + ID + ": " + label;
     }
 
+
     public String onLand(Player p) {
         String s = p.getName() + " er landet på " + this.toString();
         return s;
     }
+
     public String processResponse(Player p) {
         return "";
     }
@@ -33,4 +47,11 @@ public class Field {
         return "";
     }
 
+    public String getLabel() {
+        return label;
+    }
+
+    public int getIncome() {
+        return income;
+    }
 }
--- src/Property.java
+++ src/Property.java
@@ -1,11 +1,50 @@
-public class Property extends Field{
+public class Property extends Field {
     private int serieID;
+
     public Property(int id, String label, int cost, int income, int serieID) {
         super(id, label, cost, income);
         this.serieID = serieID;
 
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + serieID;
+    }
 
+    @Override
+    public String onLand(Player p) {
+        String msg = super.onLand(p);
+        if(owner == null){
+            this.option  = "buy";
+            msg += "Vil du købe " + super.getLabel() + "? (Y/N)";
+        }else if(owner != p){
+            this.option = "payRent";
+            msg += "Du skal betale " + super.getIncome() + ". Indforstået Y/N";
+        }
+        return msg;
+    }
+
+    @Override
+    protected String onAccept(Player p) {
+        if (this.option.equalsIgnoreCase("buy")){
+            System.out.println("Du har mulighed for at købe "+ getLabel()+ ".");
+            buyProperty();
+        }else if (this.option.equalsIgnoreCase("payRent")){
+            System.out.println("Du skal betale husleje til ejeren af "+ getLabel() + "."); //Evt ændre fra label til ejer af "Deed"
+            pay();
+        }
+        return null;
+    }
+
+    @Override
+    protected String onReject(Player p) {
+        return "";
+    }
 }
+
+
+
+
+
 
