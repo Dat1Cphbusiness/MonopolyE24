58dbb71916766aab6a9dbda41263ec81d272e029
Alissa Andrea Storm <Alissaalidib@hotmail.com>
2024-11-05 10:55:49 +0100

    Changes to bank class and customer

--- src/Game.java
+++ src/Game.java
@@ -1,34 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bank {
+public class Game {
     private String name;
-    private List<Customer> customers;
+    private List<Player> players;
     private TextUI ui;
     private FileIO io;
     private String customerDataPath;
 
-    public Bank(String name) {
+    public Game(String name) {
         this.name = name;
-        this.customers = new ArrayList<>();
+        this.players = new ArrayList<>();
         this.ui = new TextUI();
         this.io = new FileIO();
         this.customerDataPath="data/customerdata.csv";
     }
-    public void addCustomer(Customer c){
-        this.customers.add(c);
+    public void addPlayer(Player p){
+        this.players.add(p);
     }
     public String toString(){
         String s = "";
-        for (Customer c:customers) {
-            s+=c+"\n";
+        for (Player p: players) {
+            s+=p+"\n";
         }
         return s;
     }
 
 
-    public List getCustomers() {
-            return customers;
+    public List getPlayer() {
+            return players;
     }
 
     public void registerCustomer() {
@@ -39,8 +39,8 @@ public class Bank {
             String name = ui.promptText("Type name of customer:");
             int startAmount = ui.promptNumeric("Type start amount:");
 
-            Customer c = new Customer(name, startAmount);
-            this.addCustomer(c);
+            Player p = new Player(name, startAmount);
+            this.addPlayer(p);
 
             continueDialog = ui.promptText("Do you wish to create another customer?Y/N");
 
@@ -54,16 +54,16 @@ public class Bank {
                String[] values= s.split(",");
                String name = values[0];
                int balance = Integer.parseInt(values[1].trim());
-               Customer c = new Customer(name, balance);
-               customers.add(c);
+               Player c = new Player(name, balance);
+               players.add(c);
            }
        }
    }
     public void endSession(){
 
-        ArrayList<String> customersAsText = new ArrayList<>();
-        for (Customer c:customers) {
-            customersAsText.add(c.toString());
+        ArrayList<String> PlayerssAsText = new ArrayList<>();
+        for (Player p: players) {
+            PlayerssAsText.add(p.toString());
         }
         FileIO.saveData(customersAsText, this.customerDataPath, "name, balance");
     }
---
+++ src/Player.java
@@ -0,0 +1,29 @@
+
+public class Player {
+
+    private String name;
+    private int balance;
+    public Player(String name, int startAmount) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance(){
+        return this.balance;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String toString(){
+        return this.name + ", "+ this.balance;
+    }
+
+}
