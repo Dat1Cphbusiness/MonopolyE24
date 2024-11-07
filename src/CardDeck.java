b64b3bfbc8702fa4b8125333110c2bcaf0f48004
P4R4D0X <jovk1999@gmail.com>
2024-11-07 12:55:16 +0100

    + CardDeck class

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -7,19 +7,33 @@ public class CardDeck {
         this.cards = new ArrayList<Card>();
     }// end Constructor
 
-    public Card createCard(String cardData[]) {
+    public boolean createCard(String cardData[]) {
      //  for (String currentCard : cardData) {
        //String[] values = cardData[currentCard].split(",")
        //}
-        for(int i = 0; i < cardData.length; i++) {
-            String[] values = cardData[i].split(",");
-            String message = values[i];
-            int income = Integer.parseInt(values[i]);
-            int cost = Integer.parseInt(values[i]);
-            String event = values[i];
-            Card c = new Card(message, income, cost, event);
-            cards.add(c);
+
+        /*
+        try {
+            for(int i = 0; i < cardData.length; i++) {
+                String[] values = cardData[i].split(",");
+
+
+                String message = values[i];
+                int income = Integer.parseInt(values[i]);
+                int cost = Integer.parseInt(values[i]);
+                String event = values[i];
+
+                Card c = new Card(message, income, cost, event);
+                cards.add(c);
+            }   // end for-loop
+            return true;
         }
-    }
+        catch (Exception e) {
+            return false;
+        }   // end try-catch*/
+
+        new FileIO().readData("path");
+        return true;
+    }   // end createCard()
 }   // end CardDeck
 
