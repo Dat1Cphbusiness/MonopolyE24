042dcfebca6a361b6a683df2423ba2ad37ac12fd
Styrse <styrbjoern@me.com>
2024-11-08 08:34:34 +0100

    First commit

---
+++ src/Card.java
@@ -0,0 +1,7 @@
+public class Card {
+private String message;
+private int income;
+private int cost;
+private String event;
+
+}
---
+++ src/CardDeck.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class CardDeck {
+    private ArrayList<Card> cards;
+
+    public CardDeck() {
+    }
+
+    //public Card getNext(){}
+
+    public void createCards(){
+
+    }
+
+}
