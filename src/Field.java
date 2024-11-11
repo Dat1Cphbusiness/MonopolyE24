82e469d3bd5ad5aeada89c2be163024e25e6763d
Mariam <elmirmariam16@gmail.com>
2024-11-11 09:55:29 +0100

    fikset Field klassen

--- src/Field.java
+++ src/Field.java
@@ -5,17 +5,32 @@ public class Field {
     private int income;
     private String option;
 
-    public Field(String label, int id, int cost) {
+    public Field(String label, int id, int cost, int income) {
         this.label = label;
         this.id = id;
         this.cost = cost;
     }
 
+    public String processResponse (Player p){
+        return "";
+    }
+
+    protected String onAccept(Player p){
+        return "";
+    }
+
+    protected String onReject(Player p){
+        return "";
+    }
+
+
     @Override
     public String toString() {
         return "Field{" + "id=" + id + ", label='" + label + '\'' + '}';
     }
+
     public String onLand(Player p){
-        return (p.getName() + "has landed on field " + "felt ikke lavet i nu");
+        return (p.getName() + "has landed on field " + id + ", " + label);
     }
+
 }
