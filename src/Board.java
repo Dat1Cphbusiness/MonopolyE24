dc48469dc7e68ca0684825a5de280e71568595d7
Iirne <113694737+Iirne@users.noreply.github.com>
2024-11-11 09:26:58 +0100

    Sprint4 fixes  - fixed int label to string label to be correct

--- src/Board.java
+++ src/Board.java
@@ -12,7 +12,7 @@ public class Board {
         for (int i = 0; i < data.length; i++){
             String[] values = data[i].split(",");
             int id = Integer.parseInt(values[0].trim());
-            int label = Integer.parseInt(values[1].trim());
+            String label = values[1].trim();
             int cost = Integer.parseInt(values[2].trim());
             int income = Integer.parseInt(values[3].trim());
             fields[i] = new Field(id,label,cost,income);
