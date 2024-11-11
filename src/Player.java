18de784bb3a36c0052de18c944ef0012f5761428
JeppeHost <157792815+JeppeHost@users.noreply.github.com>
2024-11-11 12:27:11 +0100

    Lavet dice-klassen og tilføjet metoden rollDiceSum. Opdateret player-klassen og lavet account-klassen.

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,8 @@ public class Player {
 
     private String name;
     private int balance;
-    private final static int startBalance = 30000;
+    private int position;
+    private Account account;
 
     public Player (String name){
         this.name = name;
@@ -28,5 +29,17 @@ public class Player {
 
     public String getName() {
         return name;
+    }
+
+    public int updatePosition(int value){
+        if (position+value > 40) {
+            this.account.deposit(4000);
+        }
+        this.position += value;
+        return this.position % 40;
+    }
+
+    public void receive(int amount){
+        account.deposit(amount);
     }
 }
