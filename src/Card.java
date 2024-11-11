7f8435b24803c4ef7673930565782ad152a137cf
Alissa Andrea Storm <Alissaalidib@hotmail.com>
2024-11-11 09:36:16 +0100

    added Card class

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    private String message;
+    private int income;
+    private int cost;
+    private String event;
+
+    public Card(String message, int income, int cost, String event) {
+        this.message = message;
+        this.income = income;
+        this.cost = cost;
+        this.event = event;
+    }
+}
