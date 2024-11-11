d50fb5c17c35a7a51430c9218c0e235400c214b3
MarcusPFF <marcus.pff03@gmail.com>
2024-11-11 16:50:27 +0100

    Korrektur rettelser

--- src/Chance.java
+++ src/Chance.java
@@ -20,16 +20,16 @@ public class Chance extends Field {
         cost = card.getCost();
         income = card.getIncome();
         event = card.getEvent();
-        s += "\n Du har trukket er kort fra bunken: " + card.getMessage();
+        s += "\n Du har trukket et kort fra bunken: " + card.getMessage();
         return s;
     }
 
     public String processResponse(Player p) {
-        return "Process respons for player " + p.getName();
+        return "Behandle svar for player " + p.getName();
     }
 
     protected String onAccept(Player p) {
-        return "Player " + p.getName() + " has accepterede tilbuddet.";
+        return "Player " + p.getName() + " har accepteret tilbuddet.";
     }
 
     protected String onReject(Player p) {
