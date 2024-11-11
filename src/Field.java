44777e727d024484cc3fc3f4976ea4e510b83a0a
KongCPH <112859698+KongCPH@users.noreply.github.com>
2024-11-11 10:50:12 +0100

    ændringer i field

---
+++ src/Field.java
@@ -0,0 +1,38 @@
+public class Field {
+    private String label;
+    private int id;
+    private int cost;
+    private int income;
+    private String option;
+
+    public Field(int id, String label, int cost, int income) {
+        this.label = label;
+        this.id = id;
+        this.cost = cost;
+        this.income = income;
+    }
+
+    public String onLand(Player p){
+        return p.getName() + "has landed on" + label;
+    }
+
+    public String processResponse(Player p){
+        return null;
+    }
+
+    protected String onAccept(Player p){
+        return null;
+    }
+
+    protected String onReject(Player p){
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Field{" +
+                "id=" + id +
+                ", label='" + label + '\'' +
+                '}';
+    }
+}
