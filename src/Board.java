2c5648d8a4c1fec2b6a13a062226d964012aa402
Phils1993 <cph-pn194@cphbusiness.dk>
2024-11-11 12:08:43 +0100

    Sprint5 - Mariam & Philip Board

--- src/Board.java
+++ src/Board.java
@@ -1,25 +1,64 @@
+import java.util.ArrayList;
+
 public class Board {
     private Field[] fields;
     private CardDeck cardDeck;
+    private ArrayList<Field> propertyfields  = new ArrayList<>();
 
-    public Board(String[] fielddata, String[] carddata){
+    public Board(String[] fielddata, String[] carddata) {
         createFields(fielddata);
         this.cardDeck = new CardDeck(carddata);
     }
 
-    private void createFields(String[] data){
+    private void createFields(String[] data) {
         fields = new Field[data.length];
-        for (int i = 0; i < data.length; i++){
+        for (int i = 0; i < data.length; i++) {
             String[] values = data[i].split(",");
             int id = Integer.parseInt(values[0].trim());
             String label = values[2].trim();
             int cost = Integer.parseInt(values[3].trim());
             int income = Integer.parseInt(values[4].trim());
-            fields[i] = new Field(id,label,cost,income);
+            int serieID = Integer.parseInt(values[5].trim());
+            Field f = null;
+            String fieldtype = values[1].trim();
+
+            switch (fieldtype) {
+                case "Chance":
+                    f = new Chance(label, id, income, cost,cardDeck); // cardDeck skal måske fjernes
+                    break;
+                case " Start":
+                    f = new Start(label, id, income, cost);
+                    break;
+                case "Plot":
+                    f = new Plot(label, id, income, cost, serieID);
+                    propertyfields.add(f);
+                    break;
+                case "Brewery":
+                    f = new Brewery(label, id, income, cost, serieID);
+                    propertyfields.add(f);
+                    break;
+                case "Shipping line":
+                    f = new ShippingLine(label, id, income, cost, serieID);
+                    propertyfields.add(f);
+                    break;
+                case "Tax":
+                    f = new Tax(label, id, income, cost);
+                    break;
+                case "Prison":
+                    f = new Prison(label, id, income, cost);
+                    break;
+
+                default:
+                    System.out.println("Invalid field type");
+                    break;
+            }
+            fields[i] = f;
         }
+
     }
 
-    public Field getField(int id){
-        return fields[id-1];
+    public Field getField(int id) {
+        return fields[id - 1];
     }
+
 }
