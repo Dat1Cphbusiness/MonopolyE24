35e81e200c12df78ae19a6e923cbcfe61951138f
Styrse <styrbjoern@me.com>
2024-11-12 10:01:26 +0100

    Player Nicoline Styrbjørn

---
+++ src/Account.java
@@ -0,0 +1,25 @@
+public class Account {
+    private int balance;
+    private final static int startBalance = 30000;
+
+    public Account (){
+        this.balance = startBalance;
+            }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public boolean withdraw (int amount){
+        if (amount > balance){
+            return false;
+        } else {
+            balance -= amount;
+            return true;
+        }
+    }
+
+}
