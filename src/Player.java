68bb3a9267e80f581cd9a3645026bb929f6db075
tlirtie <142231651+tlirtie@users.noreply.github.com>
2024-11-12 12:22:00 +0100

    sprint6_player_Alexander_Nicolai

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 public class Player {
 
     private String name;
     private int position;
     Account account;
+    ArrayList<Field> deeds = new ArrayList<Field>();
+    Board board;
 
     public Player (String name){
        this(name, 0);
@@ -37,5 +41,46 @@ public class Player {
 
     public void receive(int amount){
         account.deposit(amount);
+    }
+
+    boolean pay(int amount){
+        return account.withdraw(amount);
+    }
+
+    boolean pay(int amount, Player recipient){
+        if (pay(amount)){
+            recipient.receive(amount);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean buyProperty(Field f){
+       boolean result = pay(f.cost);
+            if(result){
+                //If person has the funds to buy property it is added to their deed list
+                deeds.add(f);
+                return true;
+            }
+            return false;
+    }
+
+
+
+    public int getWorthInCash(){
+        return account.getBalance();
+    }
+
+    public int getTotalWorth(){
+        int sum = 0;
+        for (int i = 0; i < deeds.size(); i++) {
+            sum = sum + deeds.get(i).cost;
+        }
+        return sum + getWorthInCash();
+    }
+
+    public void moveToPrison(){
+        int distance = board.getField(31).id-position;
+        updatePosition(distance);
     }
 }
