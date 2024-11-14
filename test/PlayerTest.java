67cdc19f8bf386134bec9c3cdb36be7b3cd8302f
Emil_Racerbil <emildagsberg@hotmail.dk>
2024-11-14 16:49:50 +0100

    // income og cost byttey plads så det passer med rækkefølgen i data.
    30000 blev også fjernet i Playertest da de 30.000 blev lagt op i de 30.000, som allerede er der.

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -40,7 +40,7 @@ class PlayerTest {
         // til at få bygget hele spillepladen og player instanser
         Game game = new Game("Matador");
         game.setupBoard();
-        Player p = new Player("Egon");
+        Player p = new Player("Egon"); // Start amount blev fjernet, da det beløb bliver sat ind i acount, der allerede starter med 30000. Så det blir ialt 60000
         Field f = game.board.getField(40); // rådhuspladsen koster 8000
         //act
         p.buyProperty(f);
@@ -48,7 +48,7 @@ class PlayerTest {
         int expected = 22000;
 
         //assert
-        assertEquals(expected, actual);
+        assertEquals(expected, actual); // Den anden fejl findes i plot og property. Income og cost bør blive byttet, så det passer med rækkefølgen i fielddata.
 
     }
 }
