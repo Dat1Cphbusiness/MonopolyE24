c0f2e3d2ffd5c494e0cca429aa414ebfcbe45362
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2024-11-15 11:27:49 +0100

    Update Tax

--- src/Tax.java
+++ src/Tax.java
@@ -4,9 +4,22 @@ public class Tax extends Field {
     public Tax(int id, String label, int cost, int income) {
         super(id, label, cost, income);
     }
+
+
     @Override
     public String onLand(Player p) {
-        String s = super.onLand(p) + "\n Vil du betale fast beløb på" + this.cost+ "kr.? Tast Y for ja \n (Ellers trækker vi et beløb der svarer til 10% af dine aktiver)";
+        String s = null;
+
+        if (id == 5) {
+            this.option = "payFixedOrPercentage";
+            s = super.onLand(p) + "\n Vil du betale fast beløb på " + this.cost + "kr.? Tast Y for ja \n (Ellers trækker vi et beløb der svarer til 10% af dine aktiver)";
+            return s;
+        } else if (id == 39) {
+            this.option = null;
+            s = super.onLand(p) + "\n Du skal betale et fast beløb på " + this.cost + "kr.";
+            p.pay(2000);
+            return s;
+        }
         return s;
     }
 
@@ -19,7 +32,7 @@ public class Tax extends Field {
     @Override
     protected String onReject(Player p) {
         double value = (p.getWorthInCash() * 0.1);
-        p.pay((int)value);
-        return p.getName() + " har afvist, og vi trækker derfor 10% af aktiverne, som i alt bliver " + value +"kr.";
+        p.pay((int) value);
+        return p.getName() + " har afvist, og vi trækker derfor 10% af aktiverne, som i alt bliver " + value + "kr.";
     }
 }
