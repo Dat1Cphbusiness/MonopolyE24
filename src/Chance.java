03068c62b291f8477bbd257408cf786db8596ce2
Tess Gaston Løvgreen <tess@lovegreen.life>
2024-11-15 17:46:29 +0100

    refaktoring og tilpasning

--- src/Chance.java
+++ src/Chance.java
@@ -18,7 +18,7 @@ public class Chance extends Field {
         s += "\n Du har trukket et kort fra bunken: " + card.getMessage();
 
 
-        //Da der ikke findes der kort som giver spilleren en valgmulighed, bør vi bare kalde onAccept herfra.
+        //Da der ikke findes der kort som giver spilleren en valgmulighed, bør vi bare kalde onAccept herfra. Evt. omnavngiv til 'executeEvent()'
        onAccept(p);
         return s;
     }
@@ -28,39 +28,50 @@ public class Chance extends Field {
         return card.getMessage()+"," + event ;
     }
 
-  /*  public String processResponse(Player p) {
-        return "Behandle svar for spiller " + p.getName();
-    }*/
+
 
     protected String onAccept(Player p) {
+        String eventSpecificMsg = "";
         switch (event) {
             case "pay":
                 p.pay(card.getCost());
+                eventSpecificMsg = "har betalt "+card.getCost()+"kr";
                 break;
             case "reward":
                 p.receive(card.getIncome());
+                eventSpecificMsg = "har modtaget "+card.getIncome()+"kr";
                 break;
             case "rewardFromPlayers":
                 // todo: få fat i listen af spillere fra Game.
                 //  Gennemløb listen og send penge fra alle spillere (som ikke er p) til p
                 //  otherplayer.pay(card.getIncome(), p);
-
+                eventSpecificMsg = "Har modtaget betaling fra alle de andre";
                 break;
             case "prison":
                 p.moveToPrison();
+                eventSpecificMsg =" er røget i fængsel";
                 break;
 
 
             case "moveToFerry":
                 //todo skal testes
-                int b = nearestFarry(p);
-                p.setDestination(b);
-                Main.games.get(0).throwAndMove();//
-                p.setDestination(0);
+                int nearestFerryPosition = nearestFerry(p);
+
+                p.setPaymentTimes(2);//sørger for at betalingen bliver dobbelt, hvis færgen ejes af en anden spiller
+
+                moveToPosition(nearestFerryPosition, p);
+               // Main.games.get(0).throwAndMove();//vi kalder i stedet landAndAct med feltet
+                Field f = Main.games.get(0).board.getField(nearestFerryPosition);
+                Main.games.get(0).landAndAct(f);
+
+
+                p.setPaymentTimes(1);// reset betaling til normal
+
                 break;
 
+/* dobbeltkonfekt? Vi sætter paymentTimes i 'moveToFerry-casen. ( Burde være ok, hvis det med dobbeltbetaling kun sker når man rykker til en færge?)
+
             case "doubleRentPaymentToPlayer":
-                //todo skal testes - virker dobbelt. Kunne man fjerne denne case og sætte paymentTimes i den forrige case
 
                 int a = nearestFarry(p);
                 p.setPaymentTimes(2);
@@ -69,34 +80,41 @@ public class Chance extends Field {
                 p.setDestination(0);
                 p.setPaymentTimes(1);
                 break;
-
+*/
             case "moveTo":
-                if (p.getPosition() < card.getMoveToPosition()) {
-                    int m = card.getMoveToPosition() - p.getPosition();
-                    p.setDestination(m);
-                    Main.games.get(0).throwAndMove();
-                    p.setDestination(0);
-                } else {
-                    int m = Math.abs(p.getPosition() - 40) + card.getMoveToPosition();
-                    p.setDestination(m);
-                    Main.games.get(0).throwAndMove();
-                    p.setDestination(0);
-                }
+                //Vi har refaktoret beregningen ud i egen metode, moveToPosition.
+                int newPosition = card.getMoveToPosition();
+                moveToPosition(newPosition, p);
+                eventSpecificMsg = " blev rykket til "+ newPosition;
                 break;
             }
-        //todo: kan vi gøre beskeden mere specifik så den afspejler den event der lige er sket?
-        return "Spiller " + p.getName() + " har accepteret tilbuddet.";
+        // vi gør beskeden mere specifik så den afspejler den event der lige er sket
+        return "Spiller " + p.getName() + eventSpecificMsg;
             // todo: hvordan kan vi håndtere at der er trukket et kort der giver imunitet overfor fængsling?
             //  Skal Player klassen have boolean attribut (et flag) der viser om man har et wildcard? Hvor skal dette flag tjekkes henne, så man ikke ryger i fængsel hvis man har wildcard?
         }
 
+        //todo: Skal testes. denne metode bør nok ligge i Player klassen
+    private void moveToPosition(int newPosition, Player p) {
+        if (p.getPosition() <newPosition ) {
+            int m = newPosition - p.getPosition();
+            p.setDestination(m);
+            Main.games.get(0).throwAndMove();
+            p.setDestination(0);
+        } else {
+            int m = Math.abs(p.getPosition() - 40) + newPosition;
+            p.setDestination(m);
+            Main.games.get(0).throwAndMove();
+            p.setDestination(0);
+        }
+    }
 
 
-        protected String onReject (Player p){
+    protected String onReject (Player p){
         return "Spiller " + p.getName() + " afviste tilbuddet.";
         }
 
-    public int nearestFarry(Player p) {
+    public int nearestFerry(Player p) {
 
         if (p.getPosition() > 36 || p.getPosition() < 6) {
             if (p.getPosition() < 6) {
